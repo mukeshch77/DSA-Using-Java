30785755c9e42353e6ce64aa941c4fee682bddd3
Mukesh Choudhary <Mukesh.13672@stu.upes.ac.in>
2025-06-10 23:46:59 +0530

    commit

---
+++ DayOne/PrintName.java
@@ -0,0 +1,13 @@
+public class PrintName {
+    public static void main(String[] args) {
+        // Print self name using java unicode 
+        char ch1 = '\u004D';
+        char ch2 = '\u0075';
+        char ch3 = '\u006B';
+        char ch4 = '\u0065';
+        char ch5 = '\u0073';
+        char ch6 = '\u0068';
+
+        System.out.println(ch1+""+ch2+""+ch3+""+ch4+""+ch5+""+ch6);
+    }
+}
