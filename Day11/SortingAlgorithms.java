e25dfe49f8c0e344ea5434198ffc5b7f86942d83
Mukesh Choudhary <Mukesh.13672@stu.upes.ac.in>
2025-07-09 17:19:28 +0530

    commit

--- Day11/SortingAlgorithms.java
+++ Day11/SortingAlgorithms.java
@@ -1,8 +1,12 @@
 public class SortingAlgorithms {
     public static void main(String[] args) {
         int arr[] = {2, 7, 1, 5, 0, 1, 12};
+        // printArray(arr, " Original array");
+        // insertionSort(arr);   
+        // printArray(arr, " Sorted array");
+        
         printArray(arr, " Original array");
-        insertionSort(arr);   
+        selectionSort(arr);   
         printArray(arr, " Sorted array");
     }
 
@@ -31,5 +35,55 @@ public class SortingAlgorithms {
             // place the key at j+1
             arr[j+1] = key;
         }
+    }
+
+    public static void selectionSort(int arr[]){
+        int n = arr.length;
+        for(int i = 1; i < n-1; i++){
+            int minEl = arr[i];
+            int minIndex = i;
+            for(int j = i+1; j < n-1; j++){
+                if (arr[j] < minEl) {
+                    minEl = arr[j];
+                    minIndex = j;
+                }
+            }
+            swap(arr, i, minIndex);
+        }
+    }
+    
+    public static void biDirectionalselectionSort(int arr[]){
+        int n = arr.length;
+        int k = n-1;
+        for(int i = 1; i < k; i++){
+            int minEl = arr[i];
+            int minIndex = i;
+            int maxEl = arr[i];
+            int maxIndex = i;
+            for(int j = i+1; j <= k; j++){
+                if (arr[j] < minEl) {
+                    minEl = arr[j];
+                    minIndex = j;
+                } else if (arr[j] > maxEl) {
+                    maxEl = arr[j];
+                    maxIndex = j;
+                }
+            }
+            //swap min elements
+            swap(arr, i, minIndex);
+            //swap max elements
+            if (maxEl == arr[minIndex]) {
+                swap(arr, k, minIndex);
+            }else{
+                swap(arr, k, maxIndex);
+            }
+            k--;
+        }
+    }
+
+    public static void swap(int arr[], int index1, int index2){
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
     }
 }
