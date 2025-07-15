0621701566dd2e052c12c6f83e60d6945cbfab17
Mukesh Choudhary <Mukesh.13672@stu.upes.ac.in>
2025-07-15 16:46:04 +0530

    commit

---
+++ Day12/MyString.java
@@ -0,0 +1,37 @@
+public class MyString {
+    public static void main(String[] args) {
+        // String name = "Mukesh";
+        // System.out.println(name);
+
+        // int arr[] = new int[3];
+        // arr[0] = 4;
+        // System.out.println(arr[0]);
+
+        // int brr[] = arr;
+        // brr[0] = 7;
+        // System.out.println(brr[0]);
+        
+        // System.out.println(arr[0]);
+        
+        // int crr[] = new int[4];
+        // crr[0] = 6;
+
+        // System.out.println(arr[0]);
+        // System.out.println(brr[0]);
+        // System.out.println(crr[0]);
+
+        String name1 = "Mukesh";
+        System.out.println(name1);
+
+        String name2 = name1;
+        name2 = "Mukku";
+
+        System.out.println(name1);
+                
+        String name3 = "Choudharyji";
+
+        System.out.println(name1);
+        System.out.println(name2);
+        System.out.println(name3);
+    }
+}
