b5373a327801990d86ba4df8164fd6e7684c9575
Mukesh Choudhary <Mukesh.13672@stu.upes.ac.in>
2025-07-24 12:12:14 +0530

    commit

--- Day13/BracketBalancing.java
+++ Day13/BracketBalancing.java
@@ -3,7 +3,8 @@ import java.util.Stack;
 public class BracketBalancing {
     public static void main(String[] args) {
         // System.out.println(countRev("}{{}}{{{"));
-        System.out.println(countRevOpti("}{{}}{{{"));
+        // System.out.println(countRevOpti("}{{}}{{{"));
+        System.out.println(minimumNumberOfSwaps("[]]]][][[[[]"));
     }
 
     public static int countRev(String s) {
@@ -98,5 +99,21 @@ public class BracketBalancing {
         return (open + 1) / 2;
     }
 
-    
+    public static int minimumNumberOfSwaps(String s){
+        int open=0, close=0, unbalanced=0, swaps=0;
+        for (int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if (ch == '[') {
+                open++;
+                if (unbalanced > 0) {
+                    swaps += unbalanced;
+                    unbalanced--;
+                }
+            }else{
+                close++;
+                unbalanced = close - open;
+            }
+        }
+        return swaps;
+    }
 }
