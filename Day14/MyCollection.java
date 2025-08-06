69431022efe5aafcec0f4a13c122193d180557d0
Mukesh Choudhary <Mukesh.13672@stu.upes.ac.in>
2025-08-06 18:48:26 +0530

    commit

---
+++ Day14/MyCollection.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class MyCollection {
+    public static void main(String[] args) {
+        
+        List<Integer> list = new ArrayList<>();
+        System.out.println(list.size());
+    }
+}
