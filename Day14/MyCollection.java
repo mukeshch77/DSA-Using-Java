c4fe5feb0280309cdcc77dba2002ebbb332134bc
Mukesh Choudhary <Mukesh.13672@stu.upes.ac.in>
2025-08-06 19:01:21 +0530

    commit

--- Day14/MyCollection.java
+++ Day14/MyCollection.java
@@ -3,7 +3,18 @@ import java.util.*;
 public class MyCollection {
     public static void main(String[] args) {
         
-        List<Integer> list = new ArrayList<>();
-        System.out.println(list.size());
+        // List<Integer> list = new ArrayList<>();
+        // int x = 1;
+        // // list.add(Integer.valueOf(x));
+        // list.add(x);
+        // System.out.println(list);
+        
+        
+        List<int[]> list = new ArrayList<>();
+        int x[] = {1};
+        list.add(x);
+
+        System.out.println(list);
+        
     }
 }
