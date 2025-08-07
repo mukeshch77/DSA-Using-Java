70672c531056858af6c093533dffd746c06b7858
Mukesh Choudhary <Mukesh.13672@stu.upes.ac.in>
2025-08-07 11:02:50 +0530

    commit

--- Day14/MyList.java
+++ Day14/MyList.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class MyList {
     public static void main(String[] args) {
@@ -27,40 +24,51 @@ public class MyList {
         // System.out.println(list.set(0, 4));
         // System.out.println(list);
 
-        // Iterator<Integer> itr = list.iterator();
-        // System.out.println("My List -->");
-        // while (itr.hasNext()) {
-        // System.out.println(itr.next());
-        // }
+        Iterator<Integer> itr = list.iterator();
+        ListIterator<Integer> itr1 = list.listIterator();
+
+        System.out.println("My List -->");
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+
+        System.out.println("My List -->");
+        while (itr1.hasNext()) {
+            System.out.println(itr1.next());
+            if (itr1.hasPrevious()) {
+                System.out.println(itr1.previous());
+                break;
+            }
+        }
 
         // Sorting
 
-        // list.sort(new Comparator<Integer>() {
-        //     public int compare(Integer A, Integer B) {
-        //         return A - B;
-        //     }
-        // });
-        // System.out.println(list);
-        
-        List<Student> list1 = new ArrayList<>();
-        list1.add(new Student(10, "Mukesh"));
-        list1.add(new Student(10, "Rahul"));
-        list1.add(new Student(10, "Rakesh"));
-        list1.add(new Student(3, "Sim"));
-        list1.add(new Student(16, "Choudhary"));
-
-        list1.sort(new Comparator<Student>() {
-            public int compare(Student A, Student B) {
-                // return B.rollNumber - A.rollNumber;
-
-                int res = B.rollNumber - A.rollNumber;
-                if (res == 0) {
-                    return A.name.compareTo(B.name);
-                }
-                return res;
+        list.sort(new Comparator<Integer>() {
+            public int compare(Integer A, Integer B) {
+                return A - B;
             }
         });
-        System.out.println(list1);
+        System.out.println(list);
+
+        // List<Student> list1 = new ArrayList<>();
+        // list1.add(new Student(10, "Mukesh"));
+        // list1.add(new Student(10, "Rahul"));
+        // list1.add(new Student(10, "Rakesh"));
+        // list1.add(new Student(3, "Sim"));
+        // list1.add(new Student(16, "Choudhary"));
+
+        // list1.sort(new Comparator<Student>() {
+        // public int compare(Student A, Student B) {
+        // // return B.rollNumber - A.rollNumber;
+
+        // int res = B.rollNumber - A.rollNumber;
+        // if (res == 0) {
+        // return B.name.compareTo(A.name);
+        // }
+        // return res;
+        // }
+        // });
+        // System.out.println(list1);
 
     }
 }
@@ -74,7 +82,7 @@ class Student {
         this.name = name;
     }
 
-    public String toString(){
+    public String toString() {
         return name;
     }
 }
