13928a07e3987297655d7682e450fc2ab42c3bc7
Mukesh Choudhary <Mukesh.13672@stu.upes.ac.in>
2025-08-27 10:38:13 +0530

    commit

--- Day14/MyList.java
+++ Day14/MyList.java
@@ -221,16 +221,16 @@ public class MyList {
 
 
         // Queue<Integer> que = new PriorityQueue<>(Comparator.reverseOrder()); // heap
-        Queue<Integer> que = new PriorityQueue<>(Collections.reverseOrder()); // heap
+        // Queue<Integer> que = new PriorityQueue<>(Collections.reverseOrder()); // heap
         // Queue<Integer> que = new PriorityQueue<>();
 
-        que.offer(2);
-        que.offer(5);
-        que.offer(7);
-        que.offer(1);
-        que.offer(8);
-        que.offer(3);
-        que.offer(11);
+        // que.offer(2);
+        // que.offer(5);
+        // que.offer(7);
+        // que.offer(1);
+        // que.offer(8);
+        // que.offer(3);
+        // que.offer(11);
 
         System.out.println(que);
         System.out.println(que.peek());
