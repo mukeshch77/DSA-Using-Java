154a978bf777cdc90e5682a85d1fcc9d451dfbd7
Mukesh Choudhary <Mukesh.13672@stu.upes.ac.in>
2025-08-28 22:03:34 +0530

    commit

--- Day14/MyMap.java
+++ Day14/MyMap.java
@@ -6,13 +6,29 @@ public class MyMap {
         
         Map<Integer, String> map = new HashMap<>();
 
+        // map.put(1, "Mukesh");
+        // map.put(2, "Aman");
+        // System.out.println(map);
+        // map.putIfAbsent(1, "Simran");
+        // System.out.println(map);
+        // map.put(1, "Anshul");
+
+        // System.out.println(map);
+
+
         map.put(1, "Mukesh");
         map.put(2, "Aman");
+        map.put(3, "Simran");
+        map.put(4, "Anshul");
+        
         System.out.println(map);
-        map.putIfAbsent(1, "Simran");
-        System.out.println(map);
-        map.put(1, "Anshul");
 
-        System.out.println(map);
+        System.out.println(map.get(3));
+        System.out.println(map.get(5));
+        System.out.println(map.getOrDefault(5, "No one"));
+        System.out.println(map.getOrDefault(1, "No one"));
+
+        System.out.println(map.containsKey(3));
+        System.out.println(map.containsValue("Mukesh"));
     }
 }
